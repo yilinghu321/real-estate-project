6e13c8dd452d06b760ae345522735793b5d3afec
yilinghu321 <yiling.n.hu@outlook.com>
2024-03-09 17:41:25 -0800

    fix the installation problem and connected to the database

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,13 +24,14 @@ export default function Header() {
             <li className='hidden: sm:inline text-slate-700 hover:underline'>Home</li>
           </Link>
           <Link to='/about'>
-          <li className='hidden: sm:inline text-slate-700 hover:underline'>About</li>
+            <li className='hidden: sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
           <Link to='/sign-in'>
-          <li className=' text-slate-700 hover:underline'>Sign In</li>
+            <li className=' text-slate-700 hover:underline'>Sign In</li>
           </Link>
         </ul>
       </div>
     </header>
   )
 }
+ 
