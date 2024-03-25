2f0118c6574b2e1bc3ddff6f384137314fdf106c
yilinghu321 <yiling.n.hu@outlook.com>
2024-03-24 22:01:49 -0700

    complete profile page functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,21 +1,57 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice.js";
-import OAuth from "../components/OAuth.jsx";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { app } from '../firebase.js';
 
 export default function Profile() {
 
-  const [formData, setFormData] = useState({})
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
   const { currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  console.log(file);
+  console.log(uploadPercentage);
+  console.log(formData);
+  console.log(fileUploadError);
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    setFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+ 
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadPercentage(Math.round(progress));
+    },
+    (error) => {
+      setFileUploadError(error);
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then(
+        (downloadURL) => setFormData({...formData, photo: downloadURL})
+      )
+    })
+  }
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]: e.target.value,
+      [e.target.id]: e.target.value, 
     });
   };
 
@@ -45,8 +81,18 @@ export default function Profile() {
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold m-4'>Profile</h1>
-      <img className="rounded-full mx-auto h-24 w-24 mt-2 cursor-pointer" src={currentUser.photo} alt="profile" />
+      <h1 className='text-3xl text-center font-semibold m-4 mt-6'>Profile</h1>
+      <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"/>
+      <img onClick={() => fileRef.current.click()} className="rounded-full mx-auto h-24 w-24 mt-6 cursor-pointer" src={formData.photo || currentUser.photo} alt="profile" />
+      <p className="self-center mt-3">
+        {fileUploadError? 
+          <span className="text-red-500"> Failed! Error Occurs.</span> : 
+          (uploadPercentage > 0 && uploadPercentage < 100)? 
+            <span className="text-slate-500">{`Uploading ${uploadPercentage}%`}</span> :
+              (uploadPercentage === 100)? 
+                <span className="text-green-500">Successfully uploaded!</span> : ""}
+      </p>
+      
       <form onSubmit={handleSubmit} className='flex flex-col mt-6 gap-4'>
         <input type="text" 
           placeholder='username' 
