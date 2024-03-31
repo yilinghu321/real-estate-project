712feb43435cfef6eaa7af1eed79ced4cb42af4b
yilinghu321 <yiling.n.hu@outlook.com>
2024-03-31 01:50:48 -0700

    add search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -62,3 +62,74 @@ export const getListing = async(req, res, next) => {
     next(error);
   }
 }
+
+export const getListings = async (req, res, next) => {
+
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true]};
+    }
+
+    let pet = req.query.pet;
+    if (pet === undefined || pet === 'false') {
+      pet = { $in: [false, true]};
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true]};
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === 'all') {
+      type = { $in: ['rent', 'sale']};
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === 0) {
+      parking = { $gte: 0};
+    } else parking = { $gt: parking};
+    
+    let bedroom = req.query.bedroom;
+    if (bedroom === undefined || bedroom === 0) {
+      bedroom = { $gte: 0};
+    } else bedroom = { $gt: bedroom};
+    
+
+    let bathroom = req.query.bathroom;
+    if (bathroom === undefined || bathroom === 0) {
+      bathroom = { $gte: 0};
+    } else bathroom = { $gte: bathroom};
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i'},
+      offer,
+      type,
+      furnished,
+      parking,
+      pet,
+      bedroom,
+      bathroom,
+    }).sort(
+      {[sort]: order}
+    ).limit(limit).skip(startIndex);
+
+    return res.status(200).json(listings);
+
+  } catch (error) {
+    next(error);
+  }
+
+  
+  
+}
