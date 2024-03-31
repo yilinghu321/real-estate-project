4445da40db53430caf3bb35705ade09facead800
yilinghu321 <yiling.n.hu@outlook.com>
2024-03-31 15:54:23 -0700

    add onChange and onSubmit functionality to the search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(location.search);
     urlParams.set('searchTerm', searchTerm);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
