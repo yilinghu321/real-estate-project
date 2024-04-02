bcee0f025e8752bbe5332943dde94cae525821db
yilinghu321 <yiling.n.hu@outlook.com>
2024-04-02 01:29:33 -0700

    fix deploy problem

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listings', listingRouter);
 
-app.use(express.static(pth.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client','dist','index.html'));
 })
